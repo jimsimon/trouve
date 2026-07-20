a027ab8180d71ab13691c3dc82b9619236705359
github-action-benchmark <github@users.noreply.github.com>
2026-07-20 06:43:13 +0000

    add micro-benchmarks (customSmallerIsBetter) benchmark result for 847e3b0d9f9a58058130902a4ded10ee9dec875c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784529743367,
+  "lastUpdate": 1784529793769,
   "repoUrl": "https://github.com/jimsimon/trouve",
   "entries": {
     "e2e-benchmarks": [
@@ -6865,6 +6865,54 @@ window.BENCHMARK_DATA = {
             "name": "dense_query_20k_rows",
             "value": 1379764.7454545456,
             "range": "± 10441",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jim.j.simon@gmail.com",
+            "name": "Jim Simon",
+            "username": "jimsimon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "847e3b0d9f9a58058130902a4ded10ee9dec875c",
+          "message": "Tighten pull request dashboard spacing (#110)",
+          "timestamp": "2026-07-20T02:41:10-04:00",
+          "tree_id": "6fbdc97272b5c79539dc28c40246cf5b7a092b12",
+          "url": "https://github.com/jimsimon/trouve/commit/847e3b0d9f9a58058130902a4ded10ee9dec875c"
+        },
+        "date": 1784529793287,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bm25_build_5k_docs",
+            "value": 5118636.388888889,
+            "range": "± 11818",
+            "unit": "ns"
+          },
+          {
+            "name": "bm25_query_5k_docs",
+            "value": 35732.110996240604,
+            "range": "± 6",
+            "unit": "ns"
+          },
+          {
+            "name": "chunk_python_200_functions",
+            "value": 2797852.388888889,
+            "range": "± 980",
+            "unit": "ns"
+          },
+          {
+            "name": "dense_query_20k_rows",
+            "value": 1556239.0288420604,
+            "range": "± 9248",
             "unit": "ns"
           }
         ]
